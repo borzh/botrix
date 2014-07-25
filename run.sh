0025ff4430b1e714dd60b7af22840e44d560d810
borzh62 <borzh62@gmail.com>
2014-07-25 12:57:41 -0300

    Don't use full path when linking valve shared libs

--- run.sh
+++ run.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
+# Show commands that are executed.
+set -x
+
 ROOT_DIR="$(cd `dirname $0`/.. && pwd)"
+BOTRIX_DIR="$ROOT_DIR/botrix"
+BOTRIX_LIB="$BOTRIX_DIR/build/libbotrix.so"
 
 STEAM_DIR=$(echo `cat /usr/bin/steam | grep "STEAMCONFIG=" | sed -e "s/STEAMCONFIG=//"`)
 if [ "$STEAM_DIR" == "" ]; then
-    STEAM_DIR=$HOME/.steam
+    STEAM_DIR="$HOME/.steam"
 fi
 
-set -e -x
-
-# Steam is i386 (32bit).
-STEAM_RUNTIME="$HOME/.steam/ubuntu12_32/steam-runtime"
-export LD_LIBRARY_PATH="$GAME_DIR/bin:$STEAM_RUNTIME/i386/lib/i386-linux-gnu:$STEAM_RUNTIME/i386/lib:$STEAM_RUNTIME/i386/usr/lib/i386-linux-gnu:$STEAM_RUNTIME/i386/usr/lib:$STEAM_RUNTIME/amd64/lib/x86_64-linux-gnu:$STEAM_RUNTIME/amd64/lib:$STEAM_RUNTIME/amd64/usr/lib/x86_64-linux-gnu:$STEAM_RUNTIME/amd64/usr/lib:$LD_LIBRARY_PATH"
+# Fail on error.
+set -e
 
 # Check out command line parameters.
-
 if [ "$1" == "hl2dm" ]; then
     GAME_DIR="$STEAM_DIR/steam/SteamApps/common/Half-Life 2 Deathmatch"
     MOD_DIR="$STEAM_DIR/steam/SteamApps/common/Half-Life 2 Deathmatch/hl2mp"
-
-    mkdir -p "$MOD_DIR/addons" || true
-    cp "$ROOT_DIR/source-sdk-2013/mp/game/mod_hl2mp/addons"/* "$MOD_DIR/addons/"
-
-    cd "$GAME_DIR"
-    ./hl2_linux -insecure -allowdebug -novid -console -game "$MOD_DIR"
 elif [ "$1" == "srcds" ]; then
     GAME_DIR="$STEAM_DIR/steam/SteamApps/common/Source SDK Base 2013 Dedicated Server"
-    MOD_DIR="$GAME_DIR/hl2mp"
-
-    mkdir -p "$MOD_DIR/addons" || true
-    cp "$ROOT_DIR/source-sdk-2013/mp/game/mod_hl2mp/addons"/* "$MOD_DIR/addons/"
-
-    cd "$GAME_DIR"
-    ./srcds_linux -insecure -allowdebug -novid -console -game "$MOD_DIR"
+    MOD_DIR="$STEAM_DIR/steam/SteamApps/common/Half-Life 2 Deathmatch/hl2mp"
 else
     GAME_DIR="$STEAM_DIR/steam/SteamApps/common/Source SDK Base 2013 Multiplayer"
     MOD_DIR="$ROOT_DIR/source-sdk-2013/mp/game/mod_hl2mp"
+fi
 
-    cd "$GAME_DIR"
-    ./hl2_linux -game "$MOD_DIR" -debug 
+# Steam is i386 (32bit).
+STEAM_RUNTIME="$STEAM_DIR/ubuntu12_32/steam-runtime"
+export LD_LIBRARY_PATH="$GAME_DIR/bin:$MOD_DIR/bin:$STEAM_RUNTIME/i386/lib/i386-linux-gnu:$STEAM_RUNTIME/i386/lib:$STEAM_RUNTIME/i386/usr/lib/i386-linux-gnu:$STEAM_RUNTIME/i386/usr/lib:$STEAM_RUNTIME/amd64/lib/x86_64-linux-gnu:$STEAM_RUNTIME/amd64/lib:$STEAM_RUNTIME/amd64/usr/lib/x86_64-linux-gnu:$STEAM_RUNTIME/amd64/usr/lib:$LD_LIBRARY_PATH"
+
+# Prepare for launch: copy addons, libbotrix.so and botrix.cfg.
+cp -r "$BOTRIX_DIR/runtime/linux"/* "$MOD_DIR/"
+cp "$BOTRIX_LIB" "$MOD_DIR/addons/"
+
+# Launch the game.
+cd "$GAME_DIR"
+if [ "$1" == "srcds" ]; then
+    ./srcds_linux -game "$MOD_DIR" -debug
+else
+    ./hl2_linux -insecure -allowdebug -novid -console -game "$MOD_DIR"
 fi
 
